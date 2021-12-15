3e5a4c5ec8ff57a9deec2bbc3cd6644033c23241
NiprogTop <kapustin20001@gmail.com>
2021-12-15 10:37:27 +0300

    1-3

---
+++ Lesson1/ex_2/ex_2.py
@@ -0,0 +1,14 @@
+def pal(i):
+    if i == i[::-1]:
+        print('Является палиндромом')
+    else:
+        print('Не является палиндромом')
+
+
+def main():
+    word = input('Введите слово: ').lower()
+    pal(word)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Lesson1/ex_3/ex_3.py
@@ -0,0 +1,18 @@
+def simbol(i):
+    kol = 0
+    j = dict.fromkeys(i, 0)
+    for c in j:
+        j[c] += 1
+    for h in j:
+        if j[h] == 1:
+            kol += 1
+    print("Количество неповторяющихся символов: " + str(kol))
+
+
+def main():
+    word = input('Введите слово: ').lower()
+    simbol(word)
+
+
+if __name__ == '__main__':
+    main()
