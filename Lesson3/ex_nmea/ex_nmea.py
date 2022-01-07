14462d470d00851279baf9823cc386e99d8aab3c
NiProg <kapustin20001@gmail.com>
2022-01-07 11:35:59 +0300

    blank for L_3

--- Lesson3/ex_nmea/ex_nmea.py
+++ Lesson3/ex_nmea/ex_nmea.py
@@ -1,3 +1,25 @@
+"""
+NMEA-0183 is a general standard for the presentation of navigation data in text format.
+
+$GPGGA,112404.00,6003.1071858,N,03017.8198064,E,1,17,0.7,23.025,M,15.996,M,,*53
+
+$GPGGA - type of data NMEA
+112404.00 - time (hhmmss.ss)
+6003.1071858 - latitude
+N - latitude direction
+03017.8198064 - longitude
+E - direction of longitude
+"""
+
+"""
+Base: Lat: 60.051584° Lon: 30.300509°
+"""
+
+
+def read_data(filename):
+    pass
+
+
 def main():
     pass
 
