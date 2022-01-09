9cf6a8b268b169235e470fc4628f96db7ca4ccfc
NiProg <kapustin20001@gmail.com>
2022-01-09 23:34:13 +0300

    L_3 ex_nmea

--- Lesson3/ex_nmea/ex_nmea.py
+++ Lesson3/ex_nmea/ex_nmea.py
@@ -14,15 +14,71 @@ E - direction of longitude
 """
 Base: Lat: 60.051584° Lon: 30.300509°
 """
+from math import *
+
+
+def timeoutdistbetween(koorddict, base, dist, gap):
+    r_met = 6371007
+    time = 0
+    lat0 = radians(base[0])
+    lon0 = radians(base[1])
+    for key in koorddict.keys():
+        d_lat = radians(koorddict[key][0]) - lat0
+        d_lon = radians(koorddict[key][1]) - lon0
+
+        lat_norm = cos((radians(koorddict[key][0]) + lat0) / 2)
+        meters = r_met * hypot(d_lon * lat_norm, d_lat)
+        if meters <= dist:
+            time += gap
+    return time
+
+def reformdata(i):
+    deg = int(i // 100)
+    mins = int(i % 100)
+    secs = i % 1 * 60
+    wrtype = deg + mins / 60 + secs / 3600
+    return wrtype
 
 
 def read_data(filename):
-    pass
+    koorddict = {}
+    file = open(filename, "r")
+    for line in file:
+        string = (line.split(','))
+        try:
+            if string[3] == "S":
+                string[2] *= -1
+
+            if string[5] == "W":
+                string[4] *= -1
+
+            koorddict[float(string[1])] = [reformdata(float(string[2])), reformdata(float(string[4]))]
+        except:
+            pass
+
+    file.close()
+    return koorddict
+
+
+def timetransform(time):
+    s = time % 60
+    m = time // 60
+    h = time // 3600
+    return h, m, s
 
 
 def main():
-    pass
+    basecoord = [60.051584, 30.300509]
+    dist_limit = 25
+    fileName = "nmea.log"
+    # fileName = input("Enter file name, please: ")
+    gap = 0.2
+    koorddict = read_data(fileName)
+    timefarfrom = timeoutdistbetween(koorddict, basecoord, dist_limit, gap)
+    h, m, s = timetransform(int(timefarfrom))
+    print(f"Аппарат был на расстоянии менее чем 25 метров от точки"
+          f" с координатами: Lat: {basecoord[0]} Lon: {basecoord[1]} \nhour-{h:.0f} min-{m:02.0f} sec-{s:02.0f}")
 
 
 if __name__ == '__main__':
-    pass
+    main()
